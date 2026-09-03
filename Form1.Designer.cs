3ae8e6eb80ab0a5c193dfdc176353a4c56feedb3
houngmingyeak <huangmingyeak372006@gmail.com>
2026-09-03 10:57:27 +0700

    Initial commit: student score calculator form

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace student_score_hoemwork_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.txtcisco = new System.Windows.Forms.TextBox();
+            this.txtweb = new System.Windows.Forms.TextBox();
+            this.txtodbw = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.txtAvg = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myanmar Sans Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(542, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ការគ្រប់គ្រងពិន្ទុសិស្ស";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.HotPink;
+            this.label2.Location = new System.Drawing.Point(40, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(335, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "C# Programming";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.HotPink;
+            this.label3.Location = new System.Drawing.Point(40, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SQL Server";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.HotPink;
+            this.label4.Location = new System.Drawing.Point(40, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 46);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CISCO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.HotPink;
+            this.label5.Location = new System.Drawing.Point(40, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 46);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Web Dev";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.HotPink;
+            this.label6.Location = new System.Drawing.Point(40, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 46);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ODBW";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Green;
+            this.label7.Location = new System.Drawing.Point(170, 455);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 46);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.Location = new System.Drawing.Point(397, 101);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(319, 45);
+            this.txtc.TabIndex = 7;
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSQL.Location = new System.Drawing.Point(397, 164);
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(319, 45);
+            this.txtSQL.TabIndex = 8;
+            // 
+            // txtcisco
+            // 
+            this.txtcisco.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcisco.Location = new System.Drawing.Point(397, 227);
+            this.txtcisco.Name = "txtcisco";
+            this.txtcisco.Size = new System.Drawing.Size(319, 45);
+            this.txtcisco.TabIndex = 9;
+            // 
+            // txtweb
+            // 
+            this.txtweb.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtweb.Location = new System.Drawing.Point(397, 294);
+            this.txtweb.Name = "txtweb";
+            this.txtweb.Size = new System.Drawing.Size(319, 45);
+            this.txtweb.TabIndex = 10;
+            // 
+            // txtodbw
+            // 
+            this.txtodbw.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtodbw.Location = new System.Drawing.Point(397, 359);
+            this.txtodbw.Name = "txtodbw";
+            this.txtodbw.Size = new System.Drawing.Size(319, 45);
+            this.txtodbw.TabIndex = 11;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttotal.Location = new System.Drawing.Point(352, 458);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(319, 45);
+            this.txttotal.TabIndex = 12;
+            // 
+            // txtAvg
+            // 
+            this.txtAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAvg.Location = new System.Drawing.Point(352, 532);
+            this.txtAvg.Name = "txtAvg";
+            this.txtAvg.Size = new System.Drawing.Size(319, 45);
+            this.txtAvg.TabIndex = 14;
+            this.txtAvg.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Green;
+            this.label8.Location = new System.Drawing.Point(170, 529);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(174, 46);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Average";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // btnCal
+            // 
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnCal.Location = new System.Drawing.Point(266, 628);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(241, 77);
+            this.btnCal.TabIndex = 15;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 771);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtAvg);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtodbw);
+            this.Controls.Add(this.txtweb);
+            this.Controls.Add(this.txtcisco);
+            this.Controls.Add(this.txtSQL);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.TextBox txtcisco;
+        private System.Windows.Forms.TextBox txtweb;
+        private System.Windows.Forms.TextBox txtodbw;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.TextBox txtAvg;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnCal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_score_hoemwork_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            // Parse input values from text boxes
+            double CSharp = double.Parse(txtc.Text);
+            double SQL = double.Parse(txtSQL.Text);
+            double Cisco = double.Parse(txtcisco.Text);
+            double Dev = double.Parse(txtweb.Text);
+            double ODBW = double.Parse(txtodbw.Text);
+
+            // Calculate sum and average
+            double Total = CSharp + SQL + Cisco + Dev + ODBW;
+            double AVG = Total / 5.0;
+
+            // Display results
+            txttotal.Text = Total.ToString();
+            txtAvg.Text = AVG.ToString("0.00");
+        }
+    }
+}
